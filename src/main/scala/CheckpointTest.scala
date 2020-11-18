c901d266a0e9c0ab3fd21e6f116b0236978aa7be
zgg <sdut2012@163.com>
2020-11-18 23:26:08 +0800

    update

---
+++ src/main/scala/CheckpointTest.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object CheckpointTest {
+  def main(Args:Array[String]):Unit = {
+
+    val conf = new SparkConf().setAppName("checkpoint").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    sc.setCheckpointDir("src/main/resources/checkpoint/")
+
+    val rdd = sc.parallelize(List(1,2,3,4,5,6,7,8,9))
+    val res = rdd.map(x=>x+1).cache()
+    res.checkpoint()
+    res.collect().foreach(println)
+
+  }
+}
+
